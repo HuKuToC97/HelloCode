a805ceaddc370e67c44c17ec8da6513403a2948c
NiKiTa <hukuta199701@gmail.com>
2023-10-04 22:06:18 +0300

    add Example005 if else

---
+++ Example005_ConditionIfElse/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите имя пользователя:");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "никита")
+{
+    Console.WriteLine("Ура, это же НИКИТА!");
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.WriteLine(username);
+}
